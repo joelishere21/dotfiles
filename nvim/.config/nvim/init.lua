ab4422d88b76e1babbd017458127f85f987d5418
Joel Schmidt <jschmitty@protonmail.com>
2026-02-23 13:15:05 -0600

    added neovim config.

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,374 @@
+vim.cmd.colorscheme("wildcharm")
+
+-----------------------------------------------------------
+-- Bootstrap lazy.nvim
+-----------------------------------------------------------
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-----------------------------------------------------------
+-- Basic Settings
+-----------------------------------------------------------
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.winborder = "rounded"
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.showmatch = true
+vim.opt.matchtime = 2
+vim.opt.cmdheight = 1
+vim.opt.completeopt = "menuone,noinsert,noselect"
+vim.opt.showmode = true
+vim.opt.pumheight = 10
+vim.opt.pumblend = 10
+vim.opt.winblend = 0
+vim.opt.conceallevel = 0
+vim.opt.concealcursor = ""
+vim.opt.lazyredraw = true
+vim.opt.synmaxcol = 300
+vim.opt.fillchars = { eob = " " }
+
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.undofile = false
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+vim.opt.ttimeoutlen = 0
+vim.opt.autoread = true
+vim.opt.autowrite = false
+
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.backspace = "indent,eol,start"
+vim.opt.autochdir = false
+vim.opt.iskeyword:append("-")
+vim.opt.path:append("**")
+vim.opt.selection = "exclusive"
+vim.opt.mouse = "a"
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.modifiable = true
+vim.opt.encoding = "UTF-8"
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wildmenu = true
+vim.opt.wildmode = "longest:full,full"
+vim.opt.wildignore:append({ "*.o", "*.obj", "*.pyc", "*.class", "*.jar" })
+
+vim.opt.diffopt:append("linematch:60")
+
+vim.opt.redrawtime = 10000
+vim.opt.maxmempattern = 20000
+
+-----------------------------------------------------------
+-- Plugins
+-----------------------------------------------------------
+require("lazy").setup({
+
+  -- Utility
+  { "nvim-lua/plenary.nvim" },
+
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({})
+      vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+      vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+    end,
+  },
+
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    config = function()
+      require("nvim-treesitter.config").setup({
+        build = ":TSUpdate",
+        autotag = {
+          enable = true,
+        },
+        event = {
+          "BufReadPre",
+          "BufNewFile",
+        },
+        ensure_installed = {
+          "c_sharp",
+          "lua",
+          "javascript",
+          "python",
+        },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = true,
+        },
+        indent = { enable = true },
+      })
+    end,
+  },
+
+  -- Mason
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  -- Mason LSP bridge
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "omnisharp" },
+      })
+    end,
+  },
+
+  -- LSP Config
+  {
+	  "neovim/nvim-lspconfig",
+	  dependencies = {
+		  "williamboman/mason-lspconfig.nvim",
+		  "hrsh7th/cmp-nvim-lsp",
+	  },
+	  config = function()
+		  local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		  -------------------------------------------------------
+		  -- Lua LSP
+		  -------------------------------------------------------
+		  vim.lsp.config("lua_ls", {
+			  capabilities = capabilities,
+			  settings = {
+				  Lua = {
+					  diagnostics = {
+						  globals = { "vim" },
+					  },
+				  },
+			  },
+		  })
+
+		  -------------------------------------------------------
+		  -- C# LSP
+		  -------------------------------------------------------
+		  vim.lsp.config("omnisharp", {
+			  capabilities = capabilities,
+		  })
+
+		  -------------------------------------------------------
+		  -- Enable servers
+		  -------------------------------------------------------
+		  vim.lsp.enable("lua_ls")
+		  vim.lsp.enable("omnisharp")
+
+		  -------------------------------------------------------
+		  -- Keymaps
+		  -------------------------------------------------------
+		  vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+		  vim.keymap.set("n", "K", vim.lsp.buf.hover)
+		  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+		  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+	  end,
+  },
+
+  -- Completion Engine
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+          { name = "luasnip" },
+        },
+      })
+    end,
+  },
+
+  -- mini.nvim
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      require("mini.comment").setup()
+      require("mini.pairs").setup()
+    end,
+  },
+
+  -- vim-tmux-navigator
+  {
+    "christoomey/vim-tmux-navigator",
+  },
+
+})
+
+
+-----------------------------------------------------------
+-- Keymaps 
+-----------------------------------------------------------
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "Y", "y$", { desc = "Yank to end of line" })
+
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+
+vim.keymap.set("x", "<leader>p", '"_dP', { desc = "Paste without yanking" })
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete without yanking" })
+
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "L", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "H", ":bprevious<CR>", { desc = "Previous buffer" })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
+
+vim.keymap.set("n", "<C-S-j>", ":resize +2<CR>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-S-k>", ":resize -2<CR>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-S-h>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-S-l>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+vim.keymap.set("n", "<leader>e", ":Explore<CR>", { desc = "Open file explorer" })
+-- vim.keymap.set("n", "<leader>ff", ":find ", { desc = "Find file" })
+
+vim.keymap.set("n", "<leader>rc", ":e $MYVIMRC<CR>", { desc = "Edit config" })
+vim.keymap.set("n", "<leader>rl", ":so $MYVIMRC<CR>", { desc = "Reload config" })
+
+vim.keymap.set("n", "<leader>pa", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  print("file:", path)
+end, { desc = "Copy full file path" })
+
+vim.keymap.set("n", "<leader>ba", ":%bd|e#|bd#<CR>", { desc = "Close all buffers except current" })
+
+vim.keymap.set("n", "<leader>fr", function()
+  local old_name = vim.fn.expand("%")
+  local new_name = vim.fn.input("New file name: ", old_name)
+  if new_name ~= "" and new_name ~= old_name then
+    vim.cmd("saveas " .. new_name)
+    vim.fn.delete(old_name)
+    print("File renamed to: " .. new_name)
+  end
+end, { desc = "Rename current file" })
+
+vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer" })
+
+
+-----------------------------------------------------------
+-- AutoCommands 
+-----------------------------------------------------------
+local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = augroup,
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.signcolumn = "no"
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimResized", {
+  group = augroup,
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup,
+  callback = function()
+    local dir = vim.fn.expand("<afile>:p:h")
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, "p")
+    end
+  end,
+})
+
